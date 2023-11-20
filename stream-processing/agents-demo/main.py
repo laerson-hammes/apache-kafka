e455e20180f2471b8064b6eaea58e3469dd18a27
Laerson Hammes <laersonha@gmail.com>
2023-11-20 11:49:55 -0300

    Agents demo

---
+++ stream-processing/agents-demo/main.py
@@ -0,0 +1,13 @@
+import faust
+
+
+app = faust.App(
+    "agents-demo"
+)
+
+greetings_topic = app.topic("greetings", value_type=str, value_serializer='raw')
+
+@app.agent(greetings_topic)
+async def greetings_processor(stream):
+    async for greeting in stream:
+        print(f"Greeting is '{greeting}'")
